9c11aff12eb78b8b100a4ea3d6ab501c6be91470
Wenshu-Zhang <clairezhang0421@gmail.com>
2022-02-21 22:12:01 -0800

    updated the functionality of sharing a post

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -2,18 +2,36 @@ import "./share.css"
 import { MailOutline } from "@material-ui/icons"
 import { useState } from "react"
 
-export default function Share(){
-    const[anonymous, setAnonymous] = useState(false)
+export default function Share(share){
+    const [description, setDescription] = useState("");
+    const[isAnonymous, setAnonymous] = useState(false)
+
+    const descHandler = (desc)=>{
+        setDescription(desc.target.value);
+    }
+
+    const anonymousHandler = ()=>{
+        setAnonymous(!isAnonymous);
+    }
+
+    const submitHandler = (submit) => {
+        submit.preventDefault();
+        share(description, setDescription);
+      }
 
     return(
-        <div className="share">
+        <form onSubmit={submitHandler} className="share">
             <div className="shareWrapper">
                 <div className="shareTop">
-                    <div className="username"></div>
                     <MailOutline className="postIcon" htmlColor="purple"/>
                     <input 
+                    type="text"
+                    id ="newPostDesc"
+                    value={description}
                     placeholder="Share what's in your mind!" 
+                    onChange={descHandler}
                     className="shareInput"
+                    required
                     />
                 </div>
                 <hr className="shareHr"></hr>
@@ -22,12 +40,13 @@ export default function Share(){
                         make this anonymous
                         <input 
                         type="checkbox" 
+                        onChange={anonymousHandler}
                         className="checkbox">
                         </input>
                     </div>
                     <button className="shareButton">post</button>
                 </div>
             </div>
-        </div>
+        </form>
     )
 }
