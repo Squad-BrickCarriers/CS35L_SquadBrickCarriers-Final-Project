9c2dffc4ec6ec11b079bde0d932e48950a659c27
hanchen-huang <96170657+hanchen-huang@users.noreply.github.com>
2022-03-01 23:24:00 -0800

    Add like/unlike to post route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const { trimEnd } = require('lodash');
 const router = express.Router();
 const { Post, postValidation } = require('../models/Post');
+const mongoose = require('mongoose');
 
 // TODO:    1. add status code
 //          2. complete error handling
 
 // Get all posts
+// req: None
+// res: return all posts to '/' in the order of posted time
 router.get('/', async (req, res) => {
     try {
         const posts = await Post.find().sort('-postdate').populate('author', 'name');
@@ -16,6 +20,8 @@ router.get('/', async (req, res) => {
 })
 
 // Get all posts ranked by likes
+// req: None
+// res: return all posts to '/rank' in the order of likes
 router.get('/rank', async (req, res) => {
     try {
         const posts = await Post.find().sort('-likes').populate('author', 'name');
@@ -26,14 +32,12 @@ router.get('/rank', async (req, res) => {
 })
 
 // Select posts by keywords
+// req: require keyword in the body
+// res: return all posts that contain the keyword in the description 
+//      to '/search' in the order of posted time
 router.get('/search', async (req, res) => {
     try {
-        const posts = await Post.find({
-            $or: [
-                { title: { $regex: RegExp(req.body.keyword), $options: 'i' } },
-                { description: { $regex: RegExp(req.body.keyword), $options: 'i' } },
-            ]
-        })
+        const posts = await Post.find({ description: { $regex: RegExp(req.body.keyword), $options: 'i' } })
         .sort('-postdate').populate('author', 'name');
         res.json(posts);
     } catch (err) {
@@ -42,6 +46,8 @@ router.get('/search', async (req, res) => {
 })
 
 // Select a post
+// req: require postId in the parameter
+// res: return the post with the matched postId to '/:postId'
 router.get('/:postId', async (req, res) => {
     try {
         const post = await Post.findById(req.params.postId).populate('author', 'name');
@@ -52,6 +58,9 @@ router.get('/:postId', async (req, res) => {
 })
 
 // Add a post
+// req: require author(objectId), description(string), likes(integer), 
+//      anonymous(boolean) in the body
+// res: return the added post to '/'
 router.post('/', async (req, res) => {
     const { error } = postValidation(req.body);
     if (error) return res.json(error);
@@ -72,10 +81,44 @@ router.post('/', async (req, res) => {
 })
 
 // Delete a post
+// req: require postId in the parameter
+// res: return the deletecount
 router.delete('/:postId', async (req, res) => {
     try {
         const removedPost = await Post.deleteOne({ _id: req.params.postId })
         res.json(removedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+})
+
+// Like/Unlike a post
+// req: require postId in the parameter and the id in the body
+// res: return the selected post with likes and liked_users updated
+router.patch('/:postId', async (req, res) => {
+    try {
+        const userid = mongoose.Types.ObjectId(req.body.id);
+        const post = await Post.findById(req.params.postId).populate('author', 'name');
+
+        let existed = false;
+        for (let i = 0; i < post.liked_users.length; i++) {
+            var id = mongoose.Types.ObjectId(post.liked_users[i]);
+            if (id.equals(userid)) {
+                existed = true;
+                break;
+            }
+        }
+
+        if (existed) {
+            post.liked_users = post.liked_users.filter(id => !id.equals(userid));
+            post.likes--;
+        } else {
+            post.liked_users.push(userid);
+            post.likes++;
+        }
+
+        const savedPost = await post.save();
+        res.json(savedPost);
     } catch (err) {
         res.json({ message: err });
     }
